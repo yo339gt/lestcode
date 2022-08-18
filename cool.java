e3029397feafc5224498c0327b2176bdcce5563e
Yogiraj Khade <yo339gt@gmail.com>
2022-08-18 15:53:40 +0530

    inital commit

---
+++ cool.java
@@ -0,0 +1,11 @@
+public class cool{
+
+    public static void coolMethod(String args[]) {
+    
+        int a = 5;
+        int b = 6;
+    
+        int sum = a + b;
+        System.out.println("sum" + sum);
+    }
+}
